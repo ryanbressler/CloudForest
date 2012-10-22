9f852955daf948e2d0af56775dd90ddf90bed0fc
Ryan Bressler <ryanbressler@gmail.com>
2012-10-22 13:56:22 -0600

    building with feature matrix parseing

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
-package io
-package encoding/csv
 
-import "fmt"
+import (
+	"encoding/csv"
+	"flag"
+	"io"
+	"log"
+	"strings"
+	"strconv"
+	"os"
+)
 
-type Num double
+type Num float64
 
 type Splitter struct {
 	Numerical bool
@@ -14,38 +20,105 @@ type Splitter struct {
 }
 
 type Node struct {
-	Left   *Node
-	Right  *Node
-	Parent *Node
+	Left     *Node
+	Right    *Node
+	Splitter *Node
 }
 
+//this data strucutre is related to the one in rf-ace. In the future we may wish to represent
+//catagorical features usings ints to speed up hash look up etc.
 type Feature struct {
 	Data      []Num
+	Missing   []bool
 	Numerical bool
-	Mapping   map[string]Num
-	Back      map[Num]String
-	Name      String
+	Map       map[string]Num
+	Back      map[Num]string
+	Name      string
 }
 
-func ParseData(input io.Reader) []Feature {
-	data := make([]Feature,0,100)
+func NewFeature(record []string, capacity int) Feature {
+	f := Feature{make([]Num, 0, capacity), make([]bool, 0, capacity), false, make(map[string]Num, capacity), make(map[Num]string, capacity), record[0]}
+	switch record[0][0:3] {
+	case "N:":
+		//Numerical
+		f.Numerical = true
+		for i := 1; i < len(record); i++ {
+			v, err := strconv.ParseFloat(record[i],64)
+			if err != nil {
+				f.Data = append(f.Data, Num(0))
+				f.Missing = append(f.Missing, true)
+				continue
+			}
+			f.Data = append(f.Data, Num(v))
+			f.Missing = append(f.Missing, false)
+
+		}
+
+	default:
+		//Assume Catagorical
+		f.Numerical = false
+		fvalue := Num(0.0)
+		for i := 1; i < len(record); i++ {
+			v := record[i]
+			norm := strings.ToLower(v)
+			if norm == "?" || norm == "nan" || norm == "na" || norm == "null" {
+				f.Data = append(f.Data, Num(0))
+				f.Missing = append(f.Missing, true)
+				continue
+			}
+			nv, exsists := f.Map[v]
+			if exsists == false {
+				f.Map[v] = fvalue
+				f.Back[fvalue] = v
+				nv = fvalue
+				fvalue += 1.0
+			}
+			f.Data = append(f.Data, Num(nv))
+			f.Missing = append(f.Missing, false)
+
+		}
+
+	}
+	return f
+
+}
+
+func ParseAFM(input io.Reader) []Feature {
+	data := make([]Feature, 0, 100)
 	tsv := csv.NewReader(input)
 	tsv.Comma = '\t'
-	
+	_, err := tsv.Read()
+	if err == io.EOF {
+		return data
+	} else if err != nil {
+		log.Print("Error:", err)
+		return data
+	}
+	capacity := tsv.FieldsPerRecord
+
 	for {
-		line, err := r.ReadString("\n")
-		if err != nil { 
-			last = true
-		}
-		
-		if last {
+		record, err := tsv.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Print("Error:", err)
 			break
 		}
+		data = append(data, NewFeature(record, capacity))
 	}
-}
-
+	return data
 }
 
 func main() {
+	fm := flag.String("fm", "featurematrix.afm", "AFM formated feature matrix to use.")
+	flag.Parse()
+	datafile, err := os.Open(*fm) // For read access.
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer datafile.Close()
+
+	data := ParseAFM(datafile)
+	log.Print("Data file ",len(data)," by ", len(data[0].Data))
 
 }
