e8d298822124a4da187af0632e0869cf36f5fab2
Dick Kreisberg <rbkreisberg@gmail.com>
2013-03-06 02:10:50 -0800

    get splits and count interesting things

--- trees.go
+++ trees.go
@@ -49,30 +49,39 @@ func (t *Tree) AddNode(path string, pred Num, splitter *Splitter) {
 
 }
 
-
-func (t *Tree) TallySplits(fm *FeatureMatrix, fbycase *SparseCounter) []Splitter {
+func (t *Tree) GetSplits(fm *FeatureMatrix, fbycase *SparseCounter, relativeSplitCount *SparseCounter) []Splitter {
 	splitters := make([]Splitter, 0)
-	ncases := len(fm.Data[0].Data)
-	cases := make([]int, 0, ncases)
-	for i := 0; i < ncases; i++ {
-		cases = append(cases, i)
+	ncases := len(fm.Data[0].Data) // grab the number of samples for the first feature
+	cases := make([]int, ncases)   //make an array that large
+	for i, v := range cases {
+		cases[i] = i
 	}
 
 	t.Root.Recurse(func(n *Node, cases []int) {
-		if n.Left == nil && n.Right == nil {
-			leaves = append(leaves, Leaf{cases, n.Pred})
-		}
+		//if we're on a splitting node
 		if fbycase != nil && n.Splitter != nil {
-			for _, c := range cases {
-				fbycase.Add(c, fm.Map[n.Splitter.Feature], 1)
+			//add this splitter to the list
+			splitters.append(n.Splitter)
+			f := n.Splitter.Feature   //get the feature at this splitter
+			for _, c := range cases { //for each case
+
+				if f.Missing[c] == false { //if there isa value for this case
+					fbycase.Add(c, fm.Map[f], 1) //count the number of times each case is present for a split by a feature
+					fvalue = f.Back[f.Data[c]]   //what is the feature value for this case?
+
+					switch {
+					case n.Splitter.Left[fvalue]: //if the value was split to the left
+						relativeCount.Add(c, fm.Map[f], -1) //subtract one
+					case n.Splitter.Right[fvalue]:
+						relativeCount.Add(c, fm.Map[f], 1) //add one
+					}
+				}
 			}
 		}
-
 	}, fm, cases)
-	return leaves
+	return splitters //return the array of all splitters from the tree
 
 }
-
 
 func (t *Tree) GetLeaves(fm *FeatureMatrix, fbycase *SparseCounter) []Leaf {
 	leaves := make([]Leaf, 0)
@@ -83,10 +92,10 @@ func (t *Tree) GetLeaves(fm *FeatureMatrix, fbycase *SparseCounter) []Leaf {
 	}
 
 	t.Root.Recurse(func(n *Node, cases []int) {
-		if n.Left == nil && n.Right == nil {
+		if n.Left == nil && n.Right == nil { // I'm in a leaf node
 			leaves = append(leaves, Leaf{cases, n.Pred})
 		}
-		if fbycase != nil && n.Splitter != nil {
+		if fbycase != nil && n.Splitter != nil { //I'm not in a leaf node?
 			for _, c := range cases {
 				fbycase.Add(c, fm.Map[n.Splitter.Feature], 1)
 			}
