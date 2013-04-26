c672cfa66cf3c650c309770a7e506fd03e2e976f
Ryan Bressler <ryanbressler@gmail.com>
2013-04-26 14:17:07 -0600

    Add in iterative search

--- feature.go
+++ feature.go
@@ -110,12 +110,100 @@ func ParseFeature(record []string, capacity int) Feature {
 
 }
 
-/*
-BestCatSplit should be called on a catagorical feature to find the split that minimizes impurity
+/*IterBestCatSplit performs an iterative search to find the split that minimizes impurity
 in the specified target.
 
 rf-ace finds the "best" catagorical split using a greedy method that starts with the
 single best catagory, and finds the best catagory to add on each iteration.
+
+
+Searching is implmented via bitwise on intergers for speed but will currentlly only work
+when there are less catagories then the number of bits in an int.
+
+Pointers to slices for l and r and counter are used to reduce realocations during search
+and will not contain meaningfull results.
+
+l and r should have the same capacity as cases . counter is only used for catagorical targets and
+should have the same length as the number of catagories in the target.*/
+func (f *Feature) IterBestCatSplit(target *Feature, cases *[]int, l *[]int, r *[]int, counter *[]int) (bestSplit int, impurityDecrease float64) {
+
+	left := *l
+	right := *r
+	/*BUG(ryan) this won't work for n > 32. Should use bigint? or iterative search for nCats>10
+
+	This is an iterative search for the best combinations of catagories.
+	*/
+
+	nCats := len(f.Back)
+	cat := 0
+
+	//overall best impurity and split
+	impurityDecrease = minImp
+	bestSplit = 0
+
+	//best with n catagories
+	innerImp := minImp
+	innerSplit := 0
+
+	//values for the current proposed n+1 catagory
+	nextImp := minImp
+	nextSplit := 0
+
+	//Search for the best catagory to add to bestSplit
+	for j := 0; j < nCats; j++ {
+
+		//find the best additonal catagory
+		for i := 0; i < nCats; i++ {
+
+			if 0 == (bestSplit & (1 << uint(i))) {
+				left = left[0:0]
+				right = right[0:0]
+
+				nextSplit = innerSplit | 1<<uint(i)
+
+				for _, c := range *cases {
+					if f.Missing[c] == false {
+						cat = f.CatData[c]
+						if 0 != (nextSplit & (1 << uint(cat))) {
+							left = append(left, c)
+						} else {
+							right = append(right, c)
+						}
+					}
+
+				}
+
+				//skip cases where the split didn't do any splitting
+				if len(left) == 0 || len(right) == 0 {
+					continue
+				}
+
+				innerImp := target.ImpurityDecrease(&left, &right, counter)
+
+				if nextImp > innerImp {
+					innerSplit = nextSplit
+					innerImp = nextImp
+
+				}
+			}
+			if innerImp > impurityDecrease {
+				bestSplit = innerSplit
+				impurityDecrease = innerImp
+
+			} else {
+				break
+			}
+
+		}
+
+	}
+
+	return
+}
+
+/*
+BestCatSplit performs an exahustive or stoicastic search for the split that minimizes impurity
+in the specified target.
 
 This implementation follows Brieman's implementation and the R/Matlab implementations
 based on it use exsaustive search overfor when there are less thatn 25/10 catagories
@@ -295,7 +383,7 @@ func (f *Feature) BestSplit(target *Feature, cases *[]int, l *[]int, r *[]int, c
 	case true:
 		bestNum, impurityDecrease = f.BestNumSplit(target, cases, l, r, counter)
 	case false:
-		bestCat, impurityDecrease = f.BestCatSplit(target, cases, l, r, counter)
+		bestCat, impurityDecrease = f.IterBestCatSplit(target, cases, l, r, counter)
 	}
 	return
 
