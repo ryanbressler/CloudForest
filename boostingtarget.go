999bcdab325b56f1a9531dc3667941896069743a
Ryan Bressler <ryanbressler@gmail.com>
2013-05-22 21:07:16 -0600

    Adding interface and function for gradiant boosting

--- boostingtarget.go
+++ boostingtarget.go
@@ -2,11 +2,10 @@ package CloudForest
 
 import ()
 
-//Target abstracts the methods needed for a feature to be predictable
-//as either a catagroical or numerical feature in a random forest.
-type Target interface {
-	NCats() (n int)
+//BoostingTarget
+type BoostingTarget interface {
 	SplitImpurity(l []int, r []int, counter *[]int) (impurityDecrease float64)
 	Impurity(cases *[]int, counter *[]int) (impurity float64)
+	UpdateResiduals(cases *[]int)
 	FindPredicted(cases []int) (pred string)
 }
