715f56d93641d5c8b3bb9754cda87b2fe3f99230
Ryan Bressler <ryanbressler@gmail.com>
2013-10-15 18:52:08 -0600

    more effichent multithreading in leafcount

--- leafcount/leafcount.go
+++ leafcount/leafcount.go
@@ -13,9 +13,9 @@ func main() {
 	fm := flag.String("fm", "featurematrix.afm", "AFM formated feature matrix to use.")
 	rf := flag.String("rfpred", "rface.sf", "A predictor forest.")
 	outf := flag.String("leaves", "leaves.tsv", "a case by case sparse matrix of leaf co-occurrence in tsv format")
-	boutf := flag.String("branches", "branches.tsv", "a case by feature sparse matrix of leaf co-occurrence in tsv format")
-	var multithread bool
-	flag.BoolVar(&multithread, "multithread", false, "Parse seperate forests in seperate threads.")
+	boutf := flag.String("branches", "", "a case by feature sparse matrix of leaf co-occurrence in tsv format")
+	var threads int
+	flag.IntVar(&threads, "threads", 1, "Parse seperate forests in n seperate threads.")
 
 	flag.Parse()
 
@@ -28,44 +28,57 @@ func main() {
 	log.Print("Data file ", len(data.Data), " by ", data.Data[0].Length())
 
 	counts := new(CloudForest.SparseCounter)
-	caseFeatureCounts := new(CloudForest.SparseCounter)
+	var caseFeatureCounts *CloudForest.SparseCounter
+	if *boutf != "" {
+		caseFeatureCounts = new(CloudForest.SparseCounter)
+	}
 
 	files := strings.Split(*rf, ",")
 
-	if multithread {
-		runtime.GOMAXPROCS(len(files))
-	}
+	runtime.GOMAXPROCS(threads)
 
+	fileChan := make(chan string, 0)
 	doneChan := make(chan int, 0)
-	for _, fn := range files {
+
+	go func() {
+		for _, fn := range files {
+			fileChan <- fn
+		}
+	}()
+
+	for i := 0; i < threads; i++ {
 
 		go func() {
-			forestfile, err := os.Open(fn) // For read access.
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer forestfile.Close()
-			forestreader := CloudForest.NewForestReader(forestfile)
-			forest, err := forestreader.ReadForest()
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Print("Forest has ", len(forest.Trees), " trees ")
+			for {
+				fn := <-fileChan
 
-			for i := 0; i < len(forest.Trees); i++ {
-				leaves := forest.Trees[i].GetLeaves(data, caseFeatureCounts)
-				for _, leaf := range leaves {
-					for j := 0; j < len(leaf.Cases); j++ {
-						for k := 0; k < len(leaf.Cases); k++ {
+				forestfile, err := os.Open(fn) // For read access.
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer forestfile.Close()
+				forestreader := CloudForest.NewForestReader(forestfile)
+				forest, err := forestreader.ReadForest()
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Print("Forest has ", len(forest.Trees), " trees ")
 
-							counts.Add(leaf.Cases[j], leaf.Cases[k], 1)
+				for i := 0; i < len(forest.Trees); i++ {
+					leaves := forest.Trees[i].GetLeaves(data, caseFeatureCounts)
+					for _, leaf := range leaves {
+						for j := 0; j < len(leaf.Cases); j++ {
+							for k := 0; k < len(leaf.Cases); k++ {
 
+								counts.Add(leaf.Cases[j], leaf.Cases[k], 1)
+
+							}
 						}
 					}
-				}
 
+				}
+				doneChan <- 1
 			}
-			doneChan <- 1
 		}()
 
 	}
@@ -82,11 +95,13 @@ func main() {
 	defer outfile.Close()
 	counts.WriteTsv(outfile)
 
-	log.Print("Outputting Case Feature Co-Occurrence Counts")
-	boutfile, err := os.Create(*boutf) // For read access.
-	if err != nil {
-		log.Fatal(err)
+	if *boutf != "" {
+		log.Print("Outputting Case Feature Co-Occurrence Counts")
+		boutfile, err := os.Create(*boutf) // For read access.
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer boutfile.Close()
+		caseFeatureCounts.WriteTsv(boutfile)
 	}
-	defer boutfile.Close()
-	caseFeatureCounts.WriteTsv(boutfile)
 }
