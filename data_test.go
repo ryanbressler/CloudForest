27c3ea8445c80bca5302150a29d69cab4baddc62
Ryan Bressler <ryanbressler@gmail.com>
2014-03-09 22:18:34 -0600

    Better forest file testing

--- data_test.go
+++ data_test.go
@@ -532,7 +532,7 @@ func TestFileFormats(t *testing.T) {
 	cases := []int{0, 1, 2, 3, 4, 5, 6, 7}
 	candidates := []int{2, 3, 4}
 
-	fm1 := ParseARFF(strings.NewReader(irisarff))
+	fm1 := ParseAFM(strings.NewReader(fm))
 
 	go func() {
 		fm1.WriteCases(pipewriter, cases)
@@ -541,13 +541,22 @@ func TestFileFormats(t *testing.T) {
 
 	fm := ParseAFM(pipereader)
 
-	if len(fm.Data) != 5 {
-		t.Errorf("Simple feature matrix has %v features not 5", len(fm.Data))
+	if len(fm.Data) != 5 || fm.Data[0].Length() != 8 {
+		t.Errorf("Iris feature matrix has %v features and %v cases not 5 and 8", len(fm.Data), fm.Data[0].Length())
 	}
 
-	cattarget := fm.Data[4]
+	cattarget := fm.Data[1]
 	forest := GrowRandomForest(fm, cattarget.(Feature), candidates, fm.Data[0].Length(), 3, 10, 1, false, false, false, nil)
-	catvotes := NewCatBallotBox(cattarget.Length())
+
+	count := 0
+	for _, tree := range forest.Trees {
+
+		tree.Root.Recurse(func(*Node, []int, int) { count++ }, fm, cases, 0)
+
+	}
+	if count < 30 {
+		t.Errorf("Trees before send to file has only %v nodes.", count)
+	}
 
 	pipereader, pipewriter = io.Pipe()
 
@@ -561,18 +570,27 @@ func TestFileFormats(t *testing.T) {
 
 	forest, err := fr.ReadForest()
 	if err != nil {
-		t.Errorf("Error parins forest from pipe: %v", err)
+		t.Errorf("Error parseing forest from pipe: %v", err)
+	}
+	if len(forest.Trees) != 10 {
+		t.Errorf("Parsed forrest has only %v trees.", len(forest.Trees))
 	}
 
+	catvotes := NewCatBallotBox(cattarget.Length())
+	count2 := 0
 	for _, tree := range forest.Trees {
 		tree.Vote(fm, catvotes)
+		tree.Root.Recurse(func(*Node, []int, int) { count2++ }, fm, cases, 0)
+
+	}
+	if count != count2 {
+		t.Errorf("Forest before file has %v nodes differs form %v nodes after.", count, count2)
 	}
 
 	score := catvotes.TallyError(cattarget)
-	if score > 0.05 {
+	if score > 0.1 {
 		t.Errorf("Error: Classification of iris from sf file had score: %v", score)
 	}
-	t.Logf("Log: Classification of iris from sf file had score: %v", err)
 
 }
 
