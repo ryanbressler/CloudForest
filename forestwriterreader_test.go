bb039022a31ff056ee957ffc751129d6b3eb9768
Ryan Bressler <ryanbressler@gmail.com>
2014-03-09 22:24:07 -0600

    Move forest format and importance test to diffrent files.

---
+++ forestwriterreader_test.go
@@ -0,0 +1,76 @@
+package CloudForest
+
+import (
+	"io"
+	"strings"
+	"testing"
+)
+
+func TestFileFormats(t *testing.T) {
+
+	//Write out a fm and read it back in
+	pipereader, pipewriter := io.Pipe()
+	cases := []int{0, 1, 2, 3, 4, 5, 6, 7}
+	candidates := []int{2, 3, 4}
+
+	fm1 := ParseAFM(strings.NewReader(fm))
+
+	go func() {
+		fm1.WriteCases(pipewriter, cases)
+		pipewriter.Close()
+	}()
+
+	fm := ParseAFM(pipereader)
+
+	if len(fm.Data) != 5 || fm.Data[0].Length() != 8 {
+		t.Errorf("Iris feature matrix has %v features and %v cases not 5 and 8", len(fm.Data), fm.Data[0].Length())
+	}
+
+	cattarget := fm.Data[1]
+	forest := GrowRandomForest(fm, cattarget.(Feature), candidates, fm.Data[0].Length(), 3, 10, 1, false, false, false, nil)
+
+	count := 0
+	for _, tree := range forest.Trees {
+
+		tree.Root.Recurse(func(*Node, []int, int) { count++ }, fm, cases, 0)
+
+	}
+	if count < 30 {
+		t.Errorf("Trees before send to file has only %v nodes.", count)
+	}
+
+	pipereader, pipewriter = io.Pipe()
+
+	go func() {
+		fw := NewForestWriter(pipewriter)
+		fw.WriteForest(forest)
+		pipewriter.Close()
+	}()
+
+	fr := NewForestReader(pipereader)
+
+	forest, err := fr.ReadForest()
+	if err != nil {
+		t.Errorf("Error parseing forest from pipe: %v", err)
+	}
+	if len(forest.Trees) != 10 {
+		t.Errorf("Parsed forrest has only %v trees.", len(forest.Trees))
+	}
+
+	catvotes := NewCatBallotBox(cattarget.Length())
+	count2 := 0
+	for _, tree := range forest.Trees {
+		tree.Vote(fm, catvotes)
+		tree.Root.Recurse(func(*Node, []int, int) { count2++ }, fm, cases, 0)
+
+	}
+	if count != count2 {
+		t.Errorf("Forest before file has %v nodes differs form %v nodes after.", count, count2)
+	}
+
+	score := catvotes.TallyError(cattarget)
+	if score > 0.1 {
+		t.Errorf("Error: Classification of iris from sf file had score: %v", score)
+	}
+
+}
---
+++ importance_test.go
@@ -0,0 +1,111 @@
+package CloudForest
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestImportance(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping importance test on boston data set.")
+	}
+	boston := strings.NewReader(boston_housing)
+
+	fm := ParseARFF(boston)
+
+	if len(fm.Data) != 14 {
+		t.Errorf("Boston feature matrix has %v features not 14", len(fm.Data))
+	}
+
+	// add artifical contrasts
+	fm.ContrastAll()
+
+	targeti := fm.Map["class"]
+
+	candidates := make([]int, 0, 0)
+
+	for i := 0; i < len(fm.Data); i++ {
+		if i != targeti {
+			candidates = append(candidates, i)
+		}
+	}
+
+	numtarget := fm.Data[targeti]
+
+	nTrees := 20
+	//Brieman's importance definition
+	imp := func(mean float64, count float64) float64 {
+		return mean * float64(count) / float64(nTrees)
+	}
+
+	//standard
+	imppnt := NewRunningMeans(len(fm.Data))
+	_ = GrowRandomForest(fm, numtarget.(Feature), candidates, fm.Data[0].Length(), 6, nTrees, 1, false, false, false, imppnt)
+	//TODO read importance scores and verify RM and LSTAT come out on top
+
+	roomimp := imp((*imppnt)[fm.Map["RM"]].Read())
+	lstatimp := imp((*imppnt)[fm.Map["LSTAT"]].Read())
+	beatlstat := 0
+	beatroom := 0
+
+	for _, rm := range *imppnt {
+		fimp := imp(rm.Read())
+		if fimp > roomimp {
+			beatroom++
+		}
+		if fimp > lstatimp {
+			beatlstat++
+		}
+	}
+	if beatroom > 1 || beatlstat > 1 {
+		t.Error("RM and LSTAT features  not most important in boston data set regression.")
+	}
+
+	//vetting
+	imppnt = NewRunningMeans(len(fm.Data))
+	_ = GrowRandomForest(fm, numtarget.(Feature), candidates, fm.Data[0].Length(), 6, nTrees, 1, false, true, false, imppnt)
+	//TODO read importance scores and verify RM and LSTAT come out on top
+
+	roomimp = imp((*imppnt)[fm.Map["RM"]].Read())
+	lstatimp = imp((*imppnt)[fm.Map["LSTAT"]].Read())
+	beatlstat = 0
+	beatroom = 0
+
+	for _, rm := range *imppnt {
+		fimp := imp(rm.Read())
+		if fimp > roomimp {
+			beatroom++
+		}
+		if fimp > lstatimp {
+			beatlstat++
+		}
+	}
+	if beatroom > 1 || beatlstat > 1 {
+		t.Error("RM and LSTAT features  not most important in vetted boston data set regression.")
+	}
+
+	//evaloob
+	//vetting
+	imppnt = NewRunningMeans(len(fm.Data))
+	_ = GrowRandomForest(fm, numtarget.(Feature), candidates, fm.Data[0].Length(), 6, nTrees, 1, false, false, true, imppnt)
+	//TODO read importance scores and verify RM and LSTAT come out on top
+
+	roomimp = imp((*imppnt)[fm.Map["RM"]].Read())
+	lstatimp = imp((*imppnt)[fm.Map["LSTAT"]].Read())
+	beatlstat = 0
+	beatroom = 0
+
+	for _, rm := range *imppnt {
+		fimp := imp(rm.Read())
+		if fimp > roomimp {
+			beatroom++
+		}
+		if fimp > lstatimp {
+			beatlstat++
+		}
+	}
+	if beatroom > 1 || beatlstat > 1 {
+		t.Error("RM and LSTAT features  not most important in boston data set regression with eval oob.")
+	}
+
+}
