bbf2c7e13dbad7315a59966bcf2854df4bcee979
Ryan Bressler <ryanbressler@gmail.com>
2014-03-11 15:56:10 -0600

    rf-ace command added to benchmarks

--- benchmarks/benchmark.sh
+++ benchmarks/benchmark.sh
@@ -1,3 +1,4 @@
 python sklrf.py covtype.libsvm 
 growforest -train covtype.libsvm -target "0" -nTrees 50 -nCores 8 -mTry 7
 growforest -train covtypeNorm.arff -target "class" -nTrees 50 -nCores 8 -mTry 7
+#time rf-ace --trainData covtype.fm --target C:class --nodeSize 1 -n 50 -e 8 -m 7
