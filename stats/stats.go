588c49cf34de2da5d1e5dfb8f64dc61372ce0ccd
Ryan Bressler <ryanbressler@gmail.com>
2014-04-29 22:15:59 -0600

    Clean up stats documentation

--- stats/stats.go
+++ stats/stats.go
@@ -1,7 +1,6 @@
 /*
-Package stats currentelly only implements a t-test for importance score analysis in CloudForest.
-
-Some functions are ports or functions from rf-ace's math.cpp
+Package stats currentelly only implements a welch's t-test for importance score analysis
+in CloudForest.
 */
 package stats
 
@@ -24,7 +23,7 @@ func MeanAndVar(X *[]float64) (m, v, n float64) {
 
 //Ttest performs a Welch's t test and returns p-value, t-value and the degrees of freedom.
 //The p value based on the hypothesis that mean(B)>mean(A).
-//Based on code from rf-ace.
+//Based on similar code in rf-ace (Apache 2.0, Timo Erkkilä)
 func Ttest(A, B *[]float64) (p, t, v float64) {
 
 	// Calculate means and variances for each of two samples.
@@ -48,6 +47,7 @@ func Ttest(A, B *[]float64) (p, t, v float64) {
 	// This variable will store the integral of the tail of the t-distribution
 	integral := 0.0
 
+	// Comment from rf-ace:
 	// When ttrans > 0.9, we need to recast the integration in order to retain
 	// accuracy. In other words we make use of the following identity:
 	//
@@ -61,6 +61,7 @@ func Ttest(A, B *[]float64) (p, t, v float64) {
 		integral = regularizedIncompleteBeta(ttrans, v/2, 0.5)
 	}
 
+	// Comment from rf-ace:
 	// We need to be careful about which way to calculate the integral so that it represents
 	// the tail of the t-distribution. The sign of the tvalue hints which way to integrate
 	if t > 0.0 {
@@ -71,6 +72,7 @@ func Ttest(A, B *[]float64) (p, t, v float64) {
 	return
 }
 
+//Based on similar code in rf-ace (Apache 2.0, Timo Erkkilä)
 func regularizedIncompleteBeta(x, a, b float64) float64 {
 	i := 50
 	continuedFraction := 1.0
@@ -84,24 +86,27 @@ func regularizedIncompleteBeta(x, a, b float64) float64 {
 	return (math.Pow(x, a) * math.Pow(1-x, b) / (a * beta(a, b) * (1 + dO(0, x, a, b)/continuedFraction)))
 }
 
+/*
+  Even and odd factors for the infinite continued fraction representation of the
+  regularized incomplete beta function.
+  Based on similar code in rf-ace (Apache 2.0, Timo Erkkilä)
+*/
 func dO(m, x, a, b float64) float64 {
 	return (-1.0 * (a + m) * (a + b + m) * x / ((a + 2*m) * (a + 2*m + 1)))
 }
 
-/**
-  Even factors for the infinite continued fraction representation of the
-  regularized incomplete beta function
-*/
 func dE(m, x, a, b float64) float64 {
 	return (m * (b - m) * x / ((a + 2*m - 1) * (a + 2*m)))
 }
 
+//Based on similar code in rf-ace (Apache 2.0, Timo Erkkilä)
 func lgamma(x float64) float64 {
 	v, _ := math.Lgamma(x)
 	//v := math.Log(math.Abs(math.Gamma(x)))
 	return v
 }
 
+//Based on similar code in rf-ace (Apache 2.0, Timo Erkkilä)
 func beta(a, b float64) float64 {
 	return (math.Exp(lgamma(a) + lgamma(b) - lgamma(a+b)))
 }
