610826cdf3722965a4f939864f887589421bfa3f
Ryan Bressler <ryanbressler@gmail.com>
2014-05-06 13:10:00 -0600

    Tweek and Optimize regret target to implement cost minimizing decision trees

--- regrettarget.go
+++ regrettarget.go
@@ -6,6 +6,13 @@ import ()
 RegretTarget wraps a categorical feature for use in regret driven classification.
 The ith entry in costs should contain the cost of misclassifying a case that actually
 has the ith category.
+
+It is roughly equivelent to the ideas presented in:
+
+http://machinelearning.wustl.edu/mlpapers/paper_files/icml2004_LingYWZ04.pdf
+
+"Decision Trees with Minimal Costs"
+Charles X. Ling,Qiang Yang,Jianning Wang,Shichao Zhang
 */
 type RegretTarget struct {
 	CatFeature
@@ -45,28 +52,116 @@ func (target *RegretTarget) SplitImpurity(l *[]int, r *[]int, m *[]int, allocs *
 	return
 }
 
-//UpdateSImpFromAllocs willl be called when splits are being built by moving cases from r to l as in learning from numerical variables.
-//Here it just wraps SplitImpurity but it can be implemented to provide further optimization.
+//UpdateSImpFromAllocs willl be called when splits are being built by moving cases from r to l
+//to avoid recalulatign the entire split impurity.
 func (target *RegretTarget) UpdateSImpFromAllocs(l *[]int, r *[]int, m *[]int, allocs *BestSplitAllocs, movedRtoL *[]int) (impurityDecrease float64) {
-	return target.SplitImpurity(l, r, m, allocs)
+	var cat, i int
+	lcounter := *allocs.LCounter
+	rcounter := *allocs.RCounter
+	for _, i = range *movedRtoL {
+
+		//most expensive statement:
+		cat = target.Geti(i)
+		lcounter[cat]++
+		rcounter[cat]--
+		//counter[target.Geti(i)]++
+
+	}
+	nl := float64(len(*l))
+	nr := float64(len(*r))
+	nm := 0.0
+
+	impurityDecrease = nl * target.ImpFromCounts(len(*l), allocs.LCounter)
+	impurityDecrease += nr * target.ImpFromCounts(len(*r), allocs.RCounter)
+	if m != nil && len(*m) > 0 {
+		nm = float64(len(*m))
+		impurityDecrease += nm * target.ImpFromCounts(len(*m), allocs.Counter)
+	}
+
+	impurityDecrease /= nl + nr + nm
+	return
 }
 
-//RegretTarget.Impurity implements a simple regret function that finds the average cost of
-//a set using the misclassification costs in RegretTarget.Costs.
-func (target *RegretTarget) Impurity(cases *[]int, counter *[]int) (e float64) {
-	m := target.Modei(cases)
-	t := 0
-	for _, c := range *cases {
-		if target.IsMissing(c) == false {
-			t += 1
-			cat := target.Geti(c)
-			if cat != m {
-				e += target.Costs[cat]
-			}
+//FindPredicted does a mode calulation with the count of the positive/constrained
+//class corrected.
+func (target *RegretTarget) FindPredicted(cases []int) (pred string) {
+
+	mi := 0
+	mc := 0.0
+	counts := make([]int, target.NCats())
+
+	target.CountPerCat(&cases, &counts)
+
+	for cat, count := range counts {
+		cc := float64(count) * target.Costs[cat]
+		if cc > mc {
+			mi = cat
+			mc = cc
+		}
+	}
+
+	return target.NumToCat(mi)
+
+}
+
+//ImpFromCounts recalculates gini impurity from class counts for us in intertive updates.
+func (target *RegretTarget) ImpFromCounts(t int, counter *[]int) (e float64) {
+
+	mi := 0
+
+	mc := 0.0
+
+	for cat, count := range *counter {
+		cc := float64(count) * target.Costs[cat]
+
+		if cc > mc {
+			mi = cat
+			mc = cc
+		}
+
+	}
+
+	for cat, count := range *counter {
+
+		t += count
+		if cat != mi {
+			e += target.Costs[cat] * float64(count)
 		}
 
 	}
 	e /= float64(t)
 
 	return
+
 }
+
+//Impurity implements an impurity based on misslassification costs.
+func (target *RegretTarget) Impurity(cases *[]int, counter *[]int) (e float64) {
+
+	target.CountPerCat(cases, counter)
+	t := len(*cases)
+	e = target.ImpFromCounts(t, counter)
+
+	return
+
+}
+
+//RegretTarget.Impurity implements a simple regret function that finds the average cost of
+//a set using the misclassification costs in RegretTarget.Costs.
+// func (target *RegretTarget) Impurity(cases *[]int, counter *[]int) (e float64) {
+// 	m := target.Modei(cases)
+// 	t := 0
+// 	for _, c := range *cases {
+// 		if target.IsMissing(c) == false {
+// 			t += 1
+// 			cat := target.Geti(c)
+// 			if cat != m {
+// 				e += target.Costs[cat]
+// 			}
+// 		}
+
+// 	}
+// 	e /= float64(t)
+
+// 	return
+// }
