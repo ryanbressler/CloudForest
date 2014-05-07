acdb13c86cfd87b3bed4c74b6060118ccf942f1c
Ryan Bressler <ryanbressler@gmail.com>
2014-05-07 15:35:42 -0600

    dump slit values from leafcount

--- leafcount/leafcount.go
+++ leafcount/leafcount.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"github.com/ryanbressler/CloudForest"
 	"log"
 	"os"
@@ -14,10 +16,13 @@ func main() {
 	rf := flag.String("rfpred", "rface.sf", "A predictor forest.")
 	outf := flag.String("leaves", "leaves.tsv", "a case by case sparse matrix of leaf co-occurrence in tsv format")
 	boutf := flag.String("branches", "", "a case by feature sparse matrix of leaf co-occurrence in tsv format")
+	soutf := flag.String("splits", "", "a file to write a json record of splite per feature")
 	var threads int
 	flag.IntVar(&threads, "threads", 1, "Parse seperate forests in n seperate threads.")
 
 	flag.Parse()
+
+	splits := make(map[string][]string)
 
 	//Parse Data
 	data, err := CloudForest.LoadAFM(*fm)
@@ -65,6 +70,7 @@ func main() {
 				log.Print("Forest has ", len(forest.Trees), " trees ")
 
 				for i := 0; i < len(forest.Trees); i++ {
+					fmt.Print(".")
 					leaves := forest.Trees[i].GetLeaves(data, caseFeatureCounts)
 					for _, leaf := range leaves {
 						for j := 0; j < len(leaf.Cases); j++ {
@@ -74,6 +80,30 @@ func main() {
 
 							}
 						}
+					}
+
+					if *soutf != "" {
+						forest.Trees[i].Root.Climb(func(n *CloudForest.Node) {
+							if n.Splitter != nil {
+								name := n.Splitter.Feature
+								_, ok := splits[name]
+								if !ok {
+									splits[name] = make([]string, 0, 10)
+								}
+								split := ""
+								switch n.Splitter.Numerical {
+								case true:
+									split = fmt.Sprintf("%v", n.Splitter.Value)
+								case false:
+									keys := make([]string, 0, len(n.Splitter.Left))
+									for k := range n.Splitter.Left {
+										keys = append(keys, k)
+									}
+									split = strings.Join(keys, ",")
+								}
+								splits[name] = append(splits[name], split)
+							}
+						})
 					}
 
 				}
@@ -88,7 +118,7 @@ func main() {
 	}
 
 	log.Print("Outputting Case Case  Co-Occurrence Counts")
-	outfile, err := os.Create(*outf) // For read access.
+	outfile, err := os.Create(*outf)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -97,11 +127,21 @@ func main() {
 
 	if *boutf != "" {
 		log.Print("Outputting Case Feature Co-Occurrence Counts")
-		boutfile, err := os.Create(*boutf) // For read access.
+		boutfile, err := os.Create(*boutf)
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer boutfile.Close()
 		caseFeatureCounts.WriteTsv(boutfile)
+	}
+
+	if *soutf != "" {
+		soutfile, err := os.Create(*soutf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer soutfile.Close()
+		encoder := json.NewEncoder(soutfile)
+		encoder.Encode(splits)
 	}
 }
